25fbdc72a47fb2c807da622a5a9e3bcf1a7c0265
sbin <shengxiashiren@gmail.com>
2023-04-07 13:41:11 +0800

    补充提交。

--- webui.py
+++ webui.py
@@ -4,7 +4,7 @@ parser.add_argument("--port", type=int, default="7860")
 parser.add_argument("--model", type=str, default="model/fp16i8_RWKV-4-Raven-7B-v7-ChnEng-20230404-ctx2048")
 parser.add_argument("--strategy", type=str, default="cuda fp16i8")
 parser.add_argument("--listen", action='store_true', help="launch gradio with 0.0.0.0 as server name, allowing to respond to network requests")
-parser.add_argument("--cuda_on", type=str, default="1", help="RWKV_CUDA_ON value")
+parser.add_argument("--cuda_on", type=str, default="0", help="RWKV_CUDA_ON value")
 parser.add_argument("--jit_on", type=str, default="1", help="RWKV_JIT_ON value")
 cmd_opts = parser.parse_args()
 
