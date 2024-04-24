871441a2e3089a36832d21bf478b55a58107f113
sbin <shengxiashiren@gmail.com>
2024-04-24 11:09:42 +0800

    更新了readme

--- modules/mirostat.py
+++ modules/mirostat.py
@@ -32,9 +32,8 @@ class Mirostat(object):
     prob_topk = torch.softmax(sorted_logits, dim = 0)
     prev_i = torch.multinomial(prob_topk, num_samples=1, replacement=True)
     prev = sorted_indices[prev_i]
-    topk = prob_topk.cpu().numpy()
-    observed_surprise = np.mean(-np.log2(topk))
     # observed_surprise = -math.log2(prob_topk[prev_i])
+    observed_surprise = np.mean(-np.log2(prob_topk.cpu().numpy()))
     error_surprise = observed_surprise - self.tau
     self.max_surprise -= self.rate * error_surprise
     self.max_surprise = min(self.max_surprise, 4 * self.tau)
